eb5c60bea8f153911d351a86ffbee4283769450c
acejang1994 <acejang1994@gmail.com>
2015-10-15 15:01:12 -0400

    soccer

--- neato_soccer/scripts/ball_tracker.py
+++ neato_soccer/scripts/ball_tracker.py
@@ -26,14 +26,66 @@ class BallTracker(object):
         rospy.Subscriber(image_topic, Image, self.process_image)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         cv2.namedWindow('video_window')
+        cv2.setMouseCallback('video_window', self.process_mouse_event)
+        cv2.namedWindow('threshold_image')
+        # (90, 193, 21, 72, 202, 255)
+        self.red_lower_bound = 90
+        self.red_upper_bound = 193
+        self.blue_lower_bound = 21
+        self.blue_upper_bound = 72
+        self.green_lower_bound = 202
+        self.green_upper_bound = 255
+        self.center_x, self.center_y = 0, 0
+
+        cv2.createTrackbar('red lower bound', 'threshold_image', 0, 255, self.set_red_lower_bound)
+        cv2.createTrackbar('red upper bound', 'threshold_image', 0, 255, self.set_red_upper_bound)
+        cv2.createTrackbar('blue lower bound', 'threshold_image', 0, 255, self.set_blue_lower_bound)
+        cv2.createTrackbar('blue upper bound', 'threshold_image', 0, 255, self.set_blue_upper_bound)
+        cv2.createTrackbar('green lower bound', 'threshold_image', 0, 255, self.set_green_lower_bound)
+        cv2.createTrackbar('green upper bound', 'threshold_image', 0, 255, self.set_green_upper_bound)
+
+    def set_red_lower_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.red_lower_bound = val
+
+    def set_red_upper_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.red_upper_bound = val
+
+    def set_blue_lower_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.blue_lower_bound = val
+
+    def set_blue_upper_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.blue_upper_bound = val    
+
+    def set_green_lower_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.green_lower_bound = val
+
+    def set_green_upper_bound(self, val):
+        """ A callback function to handle the OpenCV slider to select the red lower bound """
+        self.green_upper_bound = val
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        print self.cv_image.shape
+        # print self.cv_image.shape
+        self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        binary_image = cv2.inRange(self.hsv_image, (self.blue_lower_bound, self.green_lower_bound, self.red_lower_bound), (self.blue_upper_bound, self.green_upper_bound, self.red_upper_bound))
+        moments = cv2.moments(binary_image)
+        if moments['m00'] != 0:
+            self.center_x, self.center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
+        cv2.circle(self.cv_image, (int(self.center_x), int(self.center_y)), 1000,(0,0,225) , 1)
+        # print self.center_x, self.center_y
         cv2.imshow('video_window', self.cv_image)
+        cv2.imshow('threshold_image', binary_image)
+        
         cv2.waitKey(5)
+
+# (90, 193, 21), (72, 202, 255)
 
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
@@ -41,6 +93,20 @@ class BallTracker(object):
         while not rospy.is_shutdown():
         	# start out not issuing any motor commands
             r.sleep()
+
+    def process_mouse_event(self, event, x,y,flags,param):
+        """ Process mouse events so that you can see the color values associated
+            with a particular pixel in the camera images """
+        image_info_window = 255*np.ones((500,500,3))
+        cv2.putText(image_info_window,
+                    'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+                    (5,50),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,0,0))
+        cv2.imshow('image_info', image_info_window)
+        cv2.waitKey(5)
+
 
 if __name__ == '__main__':
     node = BallTracker("/camera/image_raw")
